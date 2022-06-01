0449355e3ba70a7bd8ec218f92bec48c4f4e0167
Rachel Xiao <rachelxiao907@gmail.com>
2022-06-01 09:07:49 -0400

    user managementg

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,86 @@
 # 2022-05-27
 
 from flask import Flask, request, redirect, render_template, session
+import db
 
 app = Flask(__name__)
 app.secret_key = "foo"
 
+
+def logged_in():
+    return "user" in session
+
+
 @app.route("/")
 def home():
     return render_template("login.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	"""
+	Retrieves user login inputs and checks it against the "users" database table.
+	Brings user to home page after successful login.
+	"""
+	# if logged_in():
+	#     return redirect("/")
+
+	if request.method == "GET": #just getting to the page with no inputs
+		return render_template("login.html")
+
+	username = request.form["username"]
+	password = request.form["password"]
+
+	if username.strip() == "" or password.strip() == "":
+		return render_template("login.html", explain="Username or Password cannot be blank")
+
+	# Verify this user and password exists
+	user_id = db.fetch_user_id(username, password)
+	if user_id is None:
+		return render_template("login.html", explain="Username or Password is incorrect")
+
+	# Adds user and user id to session if all is well
+	session["user"] = db.fetch_username(user_id)
+	session["user_id"] = user_id
+
+	return redirect("/game")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	"""
+	Retrieves user inputs from signup page.
+	Checks it against the database to make sure the information is unique.
+	Adds information to the "users" database table.
+	"""
+	# if logged_in():
+	# 	return redirect("/")
+
+	# Default page
+	if request.method == "GET":
+		return render_template("register.html")
+
+	# Check sign up
+	user = request.form["newusername"]
+	pwd = request.form["newpassword"]
+	if user.strip() == "" or pwd.strip == "":
+		return render_template("register.html", explain="Username or Password cannot be blank")
+
+	# Add user information if passwords match
+	if (request.form["newpassword"] != request.form["newpassword1"]):
+		return render_template("register.html", explain="The passwords do not match")
+
+	register_success = db.register_user(user, pwd) #checks if not successful in the database file
+	if not register_success:
+		return render_template("register.html", explain="Username already exists")
+	else:
+		return redirect("/login")
+
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 
 if __name__ == "__main__":
     app.debug = True
---
+++ app/db.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+DB_FILE = "database.db"
+db = sqlite3.connect(DB_FILE)
+cur = db.cursor()
+
+cur.execute("""
+	CREATE TABLE IF NOT EXISTS users(
+	  id INTEGER PRIMARY KEY,
+	  username TEXT,
+	  password TEXT,
+      wins INTEGER,
+      games INTEGER)""")
+
+db.commit()
+db.close()
+
+#####################
+#                   #
+# Utility Functions #
+#                   #
+#####################
+
+
+def register_user(username, password):
+	"""
+	Tries to add the given username and password into the database.
+	Returns False if the user already exists, True if it successfully added the user.
+	"""
+	db = sqlite3.connect(DB_FILE)
+	c = db.cursor()
+
+	c.execute("SELECT * FROM users WHERE LOWER(username) = LOWER(?)", (username,))
+	row = c.fetchone()
+
+	if row is not None:
+		return False
+
+	c.execute("""INSERT INTO users(username, password, wins, games) VALUES(?, ?, ?, ?)""",(username, password, 0, 0))
+	db.commit()
+	db.close()
+	return True
+
+
+def fetch_user_id(username, password):
+	"""
+	Gets the id of the user with the given username/password combination from the database.
+	Returns None if the combination is incorrect.
+	"""
+	db = sqlite3.connect(DB_FILE)
+
+	# The following line turns the tuple into a single value (sqlite3 commands always return a tuple, even when it is one value)
+	# You can read more about row_factory in the official docs:
+	# https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.row_factory
+	db.row_factory = lambda curr, row: row[0]
+	c = db.cursor()
+
+	c.execute("""
+		SELECT id
+		FROM   users
+		WHERE  LOWER(username) = LOWER(?)
+		AND    password = ?
+	""", (username, password))
+
+	# user_id is None if no matches were found
+	user_id = c.fetchone()
+
+	db.close()
+
+	return user_id
+
+
+def fetch_username(user_id):
+	"""
+	Returns the username of the user with the given id.
+	"""
+	db = sqlite3.connect(DB_FILE)
+	db.row_factory = lambda curr, row: row[0]
+	c = db.cursor()
+
+	c.execute("SELECT username FROM users WHERE id = ?", (user_id,))
+	username = c.fetchone()
+
+	db.close()
+	return
