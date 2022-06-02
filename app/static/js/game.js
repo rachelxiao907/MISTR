ab1dfb258f4bd34f72e0664f06673e9f5df902b3
michelle.lo <mlo20@stuy.edu>
2022-06-02 09:21:50 -0400

    Chris Evans

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,7 +1,7 @@
 function display() {
 
-  var rows = 3;
-  var cols = 8;
+  var rows = 4;
+  var cols = 6;
 
   var board = new Array(rows);
   for (var i = 0; i < board.length; i++) {
@@ -13,7 +13,7 @@ function display() {
   for (var i = 0; i < board.length; i++) {
       html += "<tr>";
       for (var j = 0; j < board[i].length; j++) {
-        html += "<td> hello </td>";
+        html += "<td> <img src=\"https://cdn.britannica.com/28/215028-050-94E9EA1E/American-actor-Chris-Evans-2019.jpg\" width=\"100\"> </td>";
       }
       html += "</tr>";
   }
