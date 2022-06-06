eda8ec0dfe428ca7cef1cc569c8ea6a86cf858cd
michelle.lo <mlo20@stuy.edu>
2022-06-06 09:17:18 -0400

    added easier way to access char info

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,15 +1,24 @@
-function setup() {
+pics = ["alex.png", "anne.jpg", "bernard.png", "david.png", "paul.png", "max.png"];
+names = ["alex", "anne", "bernard", "david", "paul", "max"]
 
-  pics = ["alex.png", "anne.jpg", "bernard.png", "david.png", "paul.png", "max.png"];
+var rows = 3;
+var cols = 2;
 
-  var rows = 4;
-  var cols = 6;
+var board = new Array(rows);
+for (var i = 0; i < board.length; i++) {
+  board[i] = new Array(cols);
+}
 
-  var board = new Array(rows);
-  for (var i = 0; i < board.length; i++) {
-    board[i] = new Array(cols);
+for (var i = 0; i < board.length; i++) {
+  for (var j = 0; j < board[i].length; j++) {
+    board[i][j] = {
+      name: names[i * cols + j],
+      pic: pics[i * cols + j]
+    }
   }
+}
 
+function setup() {
   var mystery_id = 4;
   var gameboard = document.getElementById("gameboard");
   var html = "";
@@ -17,7 +26,8 @@ function setup() {
       html += "<tr>";
       for (var j = 0; j < board[i].length; j++) {
         if (j < pics.length) {
-          html += "<td> <img src=\"static/img/"+pics[i]+"\" width=\"100\"> </td>";
+          html += "<td> <img src=\"static/img/"+board[i][j].pic+"\" width=\"100\"> </td>";
+          // html += "<td> <img src=\"static/img/"+pics[i]+"\" width=\"100\"> </td>";
         }
       }
       html += "</tr>";
